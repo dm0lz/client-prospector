9b46313d1bfb09e0d88a3cf5ddf2448ae630f0fa
oducroux <ducrouxolivier@gmail.com>
2017-04-12 19:03:04 +0200

    first commit

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsBootstrap
+module ClientProspector
   class Application < Rails::Application
 
 
@@ -26,7 +26,7 @@ module RailsBootstrap
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
